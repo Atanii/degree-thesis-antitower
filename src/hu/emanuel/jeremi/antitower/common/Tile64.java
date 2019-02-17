5602c8eb5e2c49708051f5a0b06123598b9200ba
Kádár Jeremi Emánuel <h661272@stud.u-szeged.hu>
2019-02-17 13:47:11 +0100

    Felesleges sorok törlése.

--- src/hu/emanuel/jeremi/antitower/common/Tile64.java
+++ src/hu/emanuel/jeremi/antitower/common/Tile64.java
@@ -8,19 +8,12 @@ public final class Tile64 {
 	public static final int SIZE_LOG = 6;
 	public static final int PLAYERHEIGHT = SIZE>>1;
 	
-	
-	/*
-	 * 0 - 9 -- special
-	 * 
-	 */
-	
 	public static final int INSIDE =  -1;
 	public static final int OUTSIDE = -2;
 	public static final int VIRTUAL = -3;
 	
 	public static final int I = INSIDE;
 	public static final int O = OUTSIDE;
-	//public static final int V = VIRTUAL;
 	
 	public static final int DOOR_CLOSED = 18;
 	public static final int DOOR_OPENED = 19;
